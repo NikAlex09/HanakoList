c87c3e830efbd8c653f0ef9f35685a17cae085a8
NikAlex <nikolay.ivanovich.a@gmail.com>
2023-02-19 09:38:43 +0300

    Initial Commit

---
+++ HanakoList/HanakoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanakoListApp.swift
+//  HanakoList
+//
+//  Created by Николай Алексанов on 19.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct HanakoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
